579de8de914b8e16047cf1e8223178e1dd1c86f1
sheena <syeongsn@gmail.com>
2025-04-24 22:10:04 +0800

    Create sentimental-mario.py

---
+++ cs50x/Week 6/sentimental-mario.py
@@ -0,0 +1,9 @@
+from cs50 import get_int
+
+height = get_int("Height: ")
+
+while (height < 1 or height > 8):
+    height = get_int("Height: ")
+
+for i in range(height):
+    print(" " * (height - i - 1) + "#" * (i + 1) + "  " + "#" * (i + 1))
